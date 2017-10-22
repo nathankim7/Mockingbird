89b0d41034e09d0fa4130716cad29948def31750
Nathan Kim <nkseve7n@gmail.com>
2017-10-22 11:27:21 -0400

    Complete Rework

--- app.js
+++ app.js
@@ -1,50 +1,66 @@
+// require Express and Socket.io
 var express = require('express');
-var path = require('path');
 var app = express();
-var bodyParser = require('body-parser');
-
-// Define the port to run on
-app.set('port', 3000);
- 
-app.use(express.static(__dirname + '/public'));
-app.use(express.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-
-app.post('/', function(req, res) {
-    console.log(req.body);
-    res.end('OK');
-});
-
-// Listen for requests
-var server = app.listen(app.get('port'), function() {
-  var port = server.address().port;
-  console.log('Magic happens on port ' + port);
-});
-
-console.log('trace')
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+var config = require('./config.js');
 var Twit = require('twit')
 var gmc = require('@google/maps').createClient({
     key: 'AIzaSyDZIRqDmWn_wCUA7Nve57WVTM_dtSKRHJM'
-})
-
+});
 var t = new Twit({
     consumer_key: 'ObvO3l8LZnhDhssFpCJPdCZkD',
     consumer_secret: 'dgLEKJdcandsbixvtmOhWhcYc4M5j8tnKLyio0eI921MPFmI7x',
     access_token: '2427918008-GhzHyKFMIc1twxLCaDirRjeRzUIEp2YPR5ee4lr',
     access_token_secret: 'zb5FbnQcQqq1OFMB8fFak0pmlgdIdiT3sAYYbXz9UNWlQ'
-})
+});
+var curLoc;
 
-t.get('search/tweets', { q: 'since:2016-01-01', count: 10, geocode: `${43.6598948},${-79.39600940000003},0.5km` }, function(err, data, response) {
-    for (i = 0; i < data.statuses.length; i++) {
-        console.log(data.statuses[i].place)
-    }
-})
+app.set('port', (process.env.PORT || 3000));
+
+// serve the static assets (js/dashboard.js and css/dashboard.css)
+// from the public/ directory
+app.use(express.static(path.join(__dirname, 'public/')));
+
+// serve the index.html page when someone visits any of the following endpoints:
+//    1. /
+//    2. /about
+//    3. /contact
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+io.on('connection', function(socket) {
+  // a user has visited our page - add them to the visitorsData object
+  console.log('connect');
+
+  socket.on('disconnect', function() {
+    // a user has left our page - remove them from the visitorsData object
+    console.log('disconnect');
+  });
+
+  socket.on('location', function(loc) {
+      console.log(loc);
+      t.get('search/tweets', { q: 'since:2016-01-01', count: 10, geocode: `${loc.lat},${loc.lng},0.5km` }, function(err, data, response) {
+        var result = '';
+
+        for (i = 0; i < data.statuses.length; i++) {
+            console.log(data.statuses[i].text);
+            result += '<strong>' + data.statuses[i].user.screen_name + '</strong>:' + data.statuses[i].text + '<br>';
+        }
+
+        io.emit('text', result);
+      });
+  });
+});
 
 t.get('geo/search', { lat: 43.6598948, long: -79.39600940000003, granularity: 'poi', accuracy: 1000000000, max_results: 20 }, function(err, data, response) {
     for (i = 0; i < data.result.places.length; i++) {
         console.log(data.result.places[i].id + " " + data.result.places[i].full_name);
     };
 })
+
+http.listen(app.get('port'), function() {
+  console.log('listening on *:' + app.get('port'));
+});
